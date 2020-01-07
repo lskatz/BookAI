5edc4dd8008de8aaea617f1a0f3c8ad36ed2ebd0
Lee Katz <lskatz@gmail.com>
2020-01-06 19:31:47 -0500

    String::Markov

--- scripts/generate-MM.pl
+++ scripts/generate-MM.pl
@@ -5,6 +5,7 @@ use File::Basename qw/basename/;
 use Data::Dumper qw/Dumper/;
 use Getopt::Long qw/GetOptions/;
 use Text::Fuzzy;
+use String::Markov;
 
 local $0 = basename $0;
 sub logmsg{print STDERR "$0: @_\n";}
@@ -23,15 +24,6 @@ sub main{
 
   my $text = generateText($infile, $$settings{numsentences}, $settings);
 
-  #die $text;
-
-  # Cleanup
-  $text =~ s/[“”]/"/g;       # Transform double quotes
-  $text =~ s/[’]/'/g;        # Transform single quotes
-  $text =~ s/"+\s*"+//g;     # Remove empty quotes
-  $text =~ s/\s+([\.!\?,;])/$1/g;   # remove whitespace before punctuation
-  $text =~ s/ ' s /'s /g;    # Remove space in between ' s
-
   print "$text\n";
 
   return 0;
@@ -41,115 +33,12 @@ sub generateText{
   my($modelFile, $numSentences, $settings) = @_;
 
   my $model = readDumper($modelFile, $settings);
-  if($$settings{boost}){
-    boostModel($model, $$settings{boost}, $settings);
+
+  my $text = "";
+  for(1..$numSentences){
+    $text .= $model->generate_sample()." ";
   }
-
-  my @word = sort keys(%{ $$model{start} });
-  
-  # Choose a start word
-  my $startWord;
-  my $rand = rand(1);
-  my $cumulative = 0;
-  for my $possibleStartWord (@word){
-    $cumulative += $$model{start}{$possibleStartWord};
-    #logmsg "$rand <? $cumulative - $possibleStartWord";
-    if($rand < $cumulative){
-      if($possibleStartWord !~ /^\w+$/){
-        #print "Skipping - $possibleStartWord\n";
-        next;
-      }
-      $startWord = $possibleStartWord;
-      last;
-    }
-  }
-  if(!$startWord){
-    die "ERROR: no seed was found. The markov model might be corrupted.";
-  }
-
-  # This MM is punctuation-based. Generate one sentence
-  # at a time.
-  my $seed = $startWord;
-  my $generatedText = $startWord;
-  my $sentenceCounter = 0;
-  for(my $i=0; $i<$numSentences; $i++){
-    #logmsg "rep $i starts with $seed";
-    my $sentence = generateSentence($seed, $model, $settings);
-
-    if($$settings{filter}){
-      # Check sentence for quality
-      my $qmSentence = quotemeta($sentence);
-      #logmsg $qmSentence;
-      my $dictionOut = `echo "$qmSentence" | diction 2>&1`;
-      my $exit_code = $? << 8;
-      if($exit_code || $dictionOut =~ /\[/){
-        #logmsg "Sentence seemed to have warnings. Trying another. <= $sentence";
-        $numSentences++;
-        next;
-      }
-
-      my @localWords = split(/\s+/, $sentence);
-      if(@localWords > 20 || @localWords < 2){
-        $numSentences++;
-        next;
-      }
-
-      # Stop the script from short circuiting from too
-      # many filtered sentences.
-      if($numSentences > 10 * $$settings{numsentences}){
-        warn "WARNING: too many invalid sentences. Quitting early.";
-        return $generatedText;
-      }
-    }
-
-    # Add the sentence to the growing body.
-    $generatedText .= $sentence;
-    # New seed is the punctuation with $
-    $seed = substr($generatedText, -1, 1) . '$';
-  }
-
-  return $generatedText;
-}
-
-sub generateSentence{
-  my($seed, $model, $settings) = @_;
-
-  my $currentWord = $seed;
-  my $generatedText = "";
-
-  my $is_end_of_sentence = 0;
-  EACH_WORD:
-  while(! $is_end_of_sentence){
-    my $rand = rand(1);
-    my $cumulative = 0;
-    die $generatedText if(!$currentWord);
-    while(my($toWord, $freq) = each(%{ $$model{transition}{$currentWord} })){
-      # Don't count properties as things with frequency.
-      # Properties start with __ .
-      next if($toWord =~ /^__/);
-
-      #print "$rand - $cumulative - $possibleStartWord\n";
-
-      $cumulative += $freq;
-      if($rand < $cumulative){
-        $currentWord = $toWord;
-        if($currentWord =~ /\$$/){
-          $currentWord = substr($currentWord, 0, -1);
-          $generatedText .= $currentWord;
-          $is_end_of_sentence = 1;
-          last EACH_WORD;
-        }
-
-        $generatedText .= " $currentWord";
-      }
-    }
-
-  }
-
-  # First word uppercase
-  $generatedText =~ s/^(\w)/uc($1)/e;
-
-  return $generatedText;
+  return $text;
 }
 
 # If --boost, then increase the frequency of that
--- scripts/train-MM.pl
+++ scripts/train-MM.pl
@@ -6,8 +6,11 @@ use Data::Dumper qw/Dumper/;
 use Getopt::Long qw/GetOptions/;
 use Lingua::EN::Sentence qw/get_sentences add_acronyms/;
 use Text::ParseWords qw/quotewords/;
+use Clone 'clone';
+use String::Markov;
 
 local $0 = basename $0;
+sub logmsg{print STDERR "$0: @_\n";}
 exit main();
 
 sub main{
@@ -30,114 +33,33 @@ sub train{
   my $wordsCounter = 0;
 
   # MM variables
-  my(%transition, %start, $previousWord, %wordCount, $startWordsCounter);
+  my $markovChain = String::Markov->new(
+    order     => 1,
+    split_sep => qr/\s+/,
+    join_sep  => ' ',
+  );
 
-  # split the infile into 'words'
+  # Read the input file
   local $/ = undef;
   open(my $fh, '<', $infile) or die "ERROR reading file $infile: $!";
   my $text = <$fh>;
   close $fh;
   $text =~ s/\x94/ /g;
+  $text =~ s/[“”]/"/g;   # Change windows quotes
+  $text =~ s/[‘’]/'/g;   # Change windows quotes
+  $text =~ s/["\(\)]//g; # Remove parentheses
+  $text =~ s/_+//g;      # Remove italics
 
+  # split into sentences
   for my $sentence(@{ get_sentences($text) }){
-    $sentence =~ s/^\s+|\s+$//g;
+    next if($sentence =~ /^\W/);
+    $sentence =~ s/^\s+|\s+$//g; # whitespace trim
 
-    # Identify the ending punctuation of the sentence
-    my $endingPunctuation;
-    if($sentence =~ /([\.;\?!]+)$/){
-      $endingPunctuation = $1;
-      # Remove the end punctuation for later
-      $sentence = substr($sentence,0, -1 * length($endingPunctuation));
-    }
-    # Only accept sentences with punctuation
-    if(!defined($endingPunctuation)){
-      next;
-    }
-
-    # Split into words.
-    # 'Words' can also be punctuation.
-    # Whitespace information is removed.
-    my @word = quotewords('\s+', 0, $sentence);
-       @word = grep {/\S/ } 
-               map{
-                #s/\x94/ /g;            # Remove <94>
-                 s/^\s+|\s+$//g;        # trim whitespace
-                 s/[“”]/"/g;            # Change windows quotes
-                 s/[‘’]/'/g;            # Change windows quotes
-                #s/,//g;                # remove commas
-
-                 # Removing some characters is a band aid
-                 # but I don't know what to do for now
-                 s/["\(\)]//g;
-
-                 # Remove italics
-                #s/^_([a-zA-Z]+)_$/$1/;
-                 s/^_+|_+$//g;
-
-                 $_;
-               } 
-               @word;
-
-    # Don't accept sentences that start with a non-word char.
-    # Don't accept small sentences.
-    next if($word[0] =~ /^\W/);
-    next if(@word < 2);
-    
-    #print join(" ", @word)."\n";
-
-    # Record the first word as a possible seed to the MM
-    $start{$word[0]}++;
-    $startWordsCounter++;
-
-    # Record the words in the markov model
-    for my $word(@word){
-      next if($word =~ /^$/);
-
-      $wordsCounter++;
-      $wordCount{$word}++;
-
-      if(defined($previousWord)){
-        $transition{$previousWord}{$word}++;
-        $transition{$previousWord}{__count}++;
-      }
-      $previousWord = $word;
-    }
-
-    # End of sentence: next word is the punctuation.
-    # The punctuation will be notated with a $ like in suffix trees
-    my $word = $endingPunctuation.'$';
-    $transition{$previousWord}{$word}++;
-    $transition{$previousWord}{__count}++;
-    $previousWord = $word;
+    $markovChain->add_sample($sentence);
   }
 
-  # Normalize start probabilities into frequencies
-  while(my($key,$value) = each(%start)){
-    $start{$key} = $value/$startWordsCounter;
-  }
+  return $markovChain;
 
-  my @word = keys(%wordCount);
-  my $numDiffWords = scalar(@word);
-  
-  for(my $i=0; $i<$numDiffWords; $i++){
-    my $from = $word[$i];
-
-    # Change count to frequency
-    my %freq;
-    while(my($to, $count) = each(%{ $transition{$from} })){
-      if($to =~ /^__/){
-        $freq{$to} = $count;
-      } else {
-        $freq{$to} = $count / $transition{$from}{__count};
-      }
-    }
-    $transition{$from} = \%freq;
-  }
-      
-  return {
-    transition => \%transition,
-    start      => \%start,
-  };
 }
 
 sub usage{
